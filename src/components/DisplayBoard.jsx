7dd8616ce10101603c0a260331432446b2acd7e4
DK <thedavidkipnis@gmail.com>
2025-09-16 15:03:56 -0700

    Set up for sidemenu

--- src/components/DisplayBoard.jsx
+++ src/components/DisplayBoard.jsx
@@ -6,6 +6,12 @@ export default function DisplayBoard() {
     const fullText = "hi, im david.";
     const [displayedText, setDisplayedText] = useState("");
 
+    const fullText2 = "im a fullstack engineer - i build web apps.";
+    const [displayedText2, setDisplayedText2] = useState("");
+
+    const fullText3 = "i love minialism (lol).";
+    const [displayedText3, setDisplayedText3] = useState("");
+
     useEffect(() => {
         let index = 0;
         let curText = "";
@@ -17,7 +23,39 @@ export default function DisplayBoard() {
         } else {
             clearInterval(interval);
         }
-        }, 60);
+        }, 45);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        let index = 0;
+        let curText = "";
+        const interval = setInterval(() => {
+        if (index < fullText2.length) {
+            curText += fullText2[index];
+            setDisplayedText2(curText);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+        }, 20);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        let index = 0;
+        let curText = "";
+        const interval = setInterval(() => {
+        if (index < fullText3.length) {
+            curText += fullText3[index];
+            setDisplayedText3(curText);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+        }, 30);
 
         return () => clearInterval(interval);
     }, []);
@@ -25,6 +63,8 @@ export default function DisplayBoard() {
     return (
         <div className="displayboard-wrapper">
             <p className="typewriter-text">{displayedText}</p>
+            <p className="typewriter-text">{displayedText2}</p>
+            <p className="typewriter-text">{displayedText3}</p>
         </div>
     )
 }
