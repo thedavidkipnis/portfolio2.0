704033306988b9a8e1974ea639b40cd26b86906e
DK <thedavidkipnis@gmail.com>
2025-09-16 13:55:47 -0700

    Styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ import DisplayBoard from './components/DisplayBoard';
 
 export default function App() {
 
-  const [darkMode, setDarkMode] = useState(true);
-
-  useEffect(() => {
-    document.body.classList.toggle("dark-mode", darkMode);
-  }, [darkMode]);
-
   return (
     <div className="top-wrapper">
       <Navbar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,30 @@ import '../styles/Navbar.css'
 
 export default function Navbar() {
 
+    const [darkMode, setDarkMode] = useState(true);
+    
+    useEffect(() => {
+    document.body.classList.toggle("dark-mode", darkMode);
+    }, [darkMode]);
+
     return (
         <div className="navbar-wrapper">
-            
+            <button className="navbar-darkmode-button"
+                onClick={() => setDarkMode(!darkMode)}
+            >
+                <div className="navbar-darkmode-button-image-wrapper">
+                    <img
+                        src="/lightmode2.png"
+                        alt="Light Mode"
+                        className={`image ${darkMode ? "visible" : "hidden"}`}
+                    />
+                    <img
+                        src="/darkmode2.png"
+                        alt="Dark Mode"
+                        className={`image ${darkMode ? "hidden" : "visible"}`}
+                    />
+                </div>
+            </button>
         </div>
     )
 }
