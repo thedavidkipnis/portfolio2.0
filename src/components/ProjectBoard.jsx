d9ba4c7cc4d9763abc3136f61561d3952ac3caec
DK <thedavidkipnis@gmail.com>
2025-09-17 18:07:29 -0700

    Basic set up for projects board

--- src/components/ProjectBoard.jsx
+++ src/components/ProjectBoard.jsx
@@ -3,5 +3,21 @@ import TypewriterText from "./TypewriterText";
 import "../styles/ProjectBoard.css";
 
 export default function ProjectBoard() {
-  return <div className="projectboard-wrapper">hey</div>;
+
+    const projects = [
+        ["tondo", "description", "link"],
+        ["alchemystic", "description", "link"]
+    ]
+
+    return <div className="projectboard-wrapper">
+        <div className="projectboard-grid">
+            {projects.map((project, index) =>
+                <div key={index} className="projectboard-box">
+                    {project[0]}<br/>
+                    {project[1]}<br/>
+                    {project[2]}<br/>
+                </div>    
+            )}
+        </div>
+    </div>;
 }
